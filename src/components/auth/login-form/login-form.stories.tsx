0bf9f7e92b4f12b58bbb36afe35776cebd60d11a
Aleksandr Leimberg <deployapp@proton.me>
2024-08-23 05:15:58 +0100

    modified login-form

--- src/components/auth/login-form/login-form.stories.tsx
+++ src/components/auth/login-form/login-form.stories.tsx
@@ -3,20 +3,24 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { LoginForm } from './login-form'
 import {useForm} from "react-hook-form";
 
-const {
-    control,
-    register,
-    handleSubmit,
-    formState: { errors },
-} = useForm<FormValues>()
 
-console.log('errors: ', errors)
+type FormValues = {
+    email: string;
+    password: string;
+    rememberMe: boolean;
+};
 
 const meta = {
     title: 'Auth/LoginForm',
     component: LoginForm,
     tags: ['autodocs'],
 } satisfies Meta<typeof LoginForm>
+
+const {
+    formState: { errors },
+} = useForm<FormValues>()
+
+console.log('errors: ', errors)
 
 
 export default meta
